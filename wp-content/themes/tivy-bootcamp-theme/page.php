cc0b22f9b353ad476a988ae58ab00b4618fcf81a
Tivinia <tivytonga@gmail.com>
2021-04-10 08:45:16 -0600

    Remove default themes. Add my own theme.

---
+++ wp-content/themes/tivy-bootcamp-theme/page.php
@@ -0,0 +1,13 @@
+<?php
+
+    while(have_posts()) {
+        the_post(); ?>
+
+        <h1>This is a Page not a Post</h1>
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+    <?php
+    }
+
+?>
+
