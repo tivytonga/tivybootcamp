83d825b72a8c1ff527a78ce4d4ba6e5d9e32ef48
Tivinia <tivytonga@gmail.com>
2021-04-11 07:01:23 -0600

    Update meta tags and make menu dynamic.

--- wp-content/themes/tivy-bootcamp-theme/header.php
+++ wp-content/themes/tivy-bootcamp-theme/header.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
 </head>
-<body>
-<header class="site-header">
+<body <?php body_class() ?>>
+    <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
           <a href="<?php echo site_url(); ?>"><strong>Tivy's</strong> Bootcamp</a>
@@ -13,13 +15,18 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
-            <ul>
+              <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'headerMenuLocation'
+                ));
+              ?>
+            <!-- <ul>
               <li><a href="<?php echo site_url('/about-tivy'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><a href="#">Blog</a></li>
-            </ul>
+            </ul> -->
           </nav>
           <div class="site-header__util">
             <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
